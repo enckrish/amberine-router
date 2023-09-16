456c0711a2919c41bca66754ec32cd33562c8463
Krishnendu Sengupta <k.sengupta.29.11@gmail.com>
2023-09-16 17:08:29 +0530

    removed test setup code

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"context"
 	"go.mongodb.org/mongo-driver/mongo"
-	"router/pb"
-	"strings"
 )
 
 var routerDb *mongo.Database
@@ -17,29 +15,8 @@ func main() {
 		}
 	}()
 	server := NewRouterServer()
-	testSetup(server, []string{"ABC", "Docker"})
 	err := server.Start()
 	if err != nil {
 		panic(err)
-	}
-}
-
-func testSetup(server *RouterServer, services []string) {
-	const groupId = "general-analyzers"
-	const selfId = "general_analyzers-001"
-	_, err := server.Admin_setNewGroupAnalyzer(context.Background(), &pb.SetNewGroupAnalyzerReq{GroupId: groupId, NewSelfId: selfId})
-	if err != nil {
-		panic(err)
-	}
-	for _, s := range services {
-		_, err := server.Admin_setTargets(
-			context.Background(),
-			&pb.SetTargetsRequest{
-				Service: strings.ToLower(s),
-				Targets: []string{groupId},
-			})
-		if err != nil {
-			panic(err)
-		}
 	}
 }
